2701a467400d6b7a6fef80ab94f04070b50bc812
Sami Laine <sami.v.laine@gmail.com>
2018-02-14 14:36:43 +0100

    Rename config files, change log path

--- bin/real_server.sh
+++ bin/real_server.sh
@@ -2,7 +2,10 @@ if [ -z $NAME ]; then echo "Server name not set"; exit 1; fi
 
 # TODO: 2018-02-14 Disable filePatchng?
 
+CONFIGPATH=${HOME}/files/config/
+LOGPATH=${HOME}/log/
+
 echo "Starting server $NAME on `date` on port $PORT"
-./arma3server -name=$PROFILE -config=../files/config/server_${CONFIG}.cfg -port=$PORT -filePatching -enableHT -maxMem=12287 $MODS $SERVERMODS $PARAMS \
- 1>>"/home/steam/log/arma3server_${NAME}_$(date +%s).rpt" 2>>"/home/steam/log/arma3server_${NAME}_$(date +%s).rpt"
+./arma3server -name=$PROFILE -config=$CONFIGPATH/${CONFIG}.cfg -port=$PORT -filePatching -enableHT -maxMem=12287 $MODS $SERVERMODS $PARAMS \
+ 1>>"$LOGPATH/${NAME}_$(date +%s).rpt" 2>>"$LOGPATH/${NAME}_$(date +%s).rpt"
 
