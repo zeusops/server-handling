8ce7ba587caf722c726787ee8871c6ff978f138e
Sami Laine <sami.v.laine@gmail.com>
2018-02-24 23:56:42 +0100

    Clean log file names

--- bin/real_server.sh
+++ bin/real_server.sh
@@ -10,7 +10,5 @@ cd $BASEPATH/arma3
 
 echo "Starting server $NAME on `date` on port $PORT"
 ./arma3server -name=$PROFILE -config=$CONFIGPATH/${CONFIG}.cfg -port=$PORT -filePatching -enableHT -maxMem=12287 $MODS $SERVERMODS $PARAMS \
- 1>>"$LOGPATH/${NAME}_$(date +%s).rpt" 2>>"$LOGPATH/${NAME}_$(date +%s).rpt"
- 1>>"/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt" 2>>"/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt"
-# tail -f "/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt" | grep -v "base class"
+ 1>>"$LOGPATH/${NAME}_$(date +%F_%H-%M-%S).rpt" 2>>"$LOGPATH/${NAME}_$(date +%F_%H-%M-%S).rpt"
 
