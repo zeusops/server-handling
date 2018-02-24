7131ae58252e9b84a9da57d0ead7ba0496e191ad
Sami Laine <sami.v.laine@gmail.com>
2018-02-24 23:56:13 +0100

    Use basepath in real_server

--- bin/real_server.sh
+++ bin/real_server.sh
@@ -2,10 +2,15 @@ if [ -z $NAME ]; then echo "Server name not set"; exit 1; fi
 
 # TODO: 2018-02-14 Disable filePatchng?
 
-CONFIGPATH=${HOME}/files/config/
-LOGPATH=${HOME}/log/
+BASEPATH=$HOME
+CONFIGPATH=$BASEPATH/files/config/
+LOGPATH=$BASEPATH/log/
+
+cd $BASEPATH/arma3
 
 echo "Starting server $NAME on `date` on port $PORT"
 ./arma3server -name=$PROFILE -config=$CONFIGPATH/${CONFIG}.cfg -port=$PORT -filePatching -enableHT -maxMem=12287 $MODS $SERVERMODS $PARAMS \
  1>>"$LOGPATH/${NAME}_$(date +%s).rpt" 2>>"$LOGPATH/${NAME}_$(date +%s).rpt"
+ 1>>"/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt" 2>>"/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt"
+# tail -f "/home/steam/test/log/arma3server_${NAME}_$(date +%s).rpt" | grep -v "base class"
 
