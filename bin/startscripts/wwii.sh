b7f75a7d776cc6d1f0191976a7f0515e81842382
Sami Laine <sami.v.laine@gmail.com>
2018-02-26 23:07:29 +0100

    Add WWII campaign server

---
+++ bin/startscripts/wwii.sh
@@ -0,0 +1,12 @@
+NAME=wwii
+PORT=2312
+
+PROFILE=  # default: server_main
+CONFIG=  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
