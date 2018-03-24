0136f4f2938d35956b4237c2ee3c90b406a8de63
Sami Laine <sami.v.laine@gmail.com>
2018-03-25 00:26:25 +0100

    Add Linnarsson's sideop for 2018-03-27

---
+++ servers/linnarsson.sh
@@ -0,0 +1,12 @@
+NAME=linnarsson
+PORT=2312
+
+PROFILE=  # default: server_main
+CONFIG=sideop  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
