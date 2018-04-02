45aa353e0e8129ab79c901518983820b400b71ac
Sami Laine <sami.v.laine@gmail.com>
2018-04-02 15:48:50 +0200

    Add server configuration for sideop with regular mods

---
+++ servers/sideop.sh
@@ -0,0 +1,12 @@
+NAME=sideop
+PORT=2302
+
+PROFILE=  # default: server_main
+CONFIG=  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
