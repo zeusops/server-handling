5b5a35d52d31e4db7a3e354dac7c28e295e2f474
Sami Laine <sami.v.laine@gmail.com>
2018-04-25 11:51:52 +0200

    Disable mod linking when updating all mods

--- bin/update_all.sh
+++ bin/update_all.sh
@@ -1,41 +1,51 @@
 set -e
 
-MODIDS=$HOME/modids.txt
+MODLISTS=$HOME/files/modlists
+ALLMODIDS=/tmp/modids.txt
 JOINTOPIDS=$HOME/jointopids.txt
 STEAMDIR=$HOME/.steam/steamcmd
 INSTALLDIR=$STEAMDIR/mods
-ARMADIR=$HOME/arma3
-MODS=$ARMADIR/mods
-UPDATEDKEYS=$ARMADIR/updated_keys
-JOINTOPKEYS=$HOME/test/arma3/available_keys/jointop
-BACKUPMODS=$ARMADIR/backupmods
-JOINTOPMODS=$HOME/test/arma3/mods/jointop
+#ARMADIR=$HOME/arma3
+#MODS=$ARMADIR/mods
+#UPDATEDKEYS=$ARMADIR/updated_keys
+#JOINTOPKEYS=$HOME/test/arma3/available_keys/jointop
+#BACKUPMODS=$ARMADIR/backupmods
+#JOINTOPMODS=$HOME/test/arma3/mods/jointop
+STEAMCMD=/usr/games/steamcmd
+LOWERCASE=$HOME/files/bin/internal/lowercase_single.sh
 
-if [ "$1" == "--jointop" ]; then
-	MODIDS=$JOINTOPIDS
-        MODS=$JOINTOPMODS
-        UPDATEDKEYS=$JOINTOPKEYS
-fi
+#if [ "$1" == "--jointop" ]; then
+#	MODIDS=$JOINTOPIDS
+#        MODS=$JOINTOPMODS
+#        UPDATEDKEYS=$JOINTOPKEYS
+#fi
 
+sort $MODLISTS/* -u > $ALLMODIDS
 
 ALLMODS=""
 while read line; do
 	ARRAY=($line)
 	MOD=${ARRAY[0]}
 	ALLMODS="$ALLMODS +workshop_download_item 107410 $MOD"
-done < $MODIDS
+done < $ALLMODIDS
+
+echo $ALLMODS
+echo $INSTALLDIR
 
 echo "Updating mods"
-steamcmd +login zeusoperations +force_install_dir $INSTALLDIR $ALLMODS +quit
+$STEAMCMD +login zeusoperations +force_install_dir $INSTALLDIR $ALLMODS +quit
+
 while read line; do
 	ARRAY=($line)
 	MODID=${ARRAY[0]}
-	MODNAME=${ARRAY[1]}
-	if [ -e $MODS/$MODNAME ]; then
-		rm $MODS/$MODNAME
-	fi
-	ln -sv $INSTALLDIR/steamapps/workshop/content/107410/$MODID $MODS/$MODNAME
-	lowercase_single.sh $MODS/$MODNAME/
-	find $MODS/$MODNAME/ -type f -exec chmod -x {} \;
-	find $MODS/$MODNAME/ -iname "*.bikey" -exec ln -sv {} $UPDATEDKEYS/ \;
-done < $MODIDS
+#	MODNAME=${ARRAY[1]}
+#	if [ -e $MODS/$MODNAME ]; then
+#		rm $MODS/$MODNAME
+#	fi
+#	ln -sv $INSTALLDIR/steamapps/workshop/content/107410/$MODID $MODS/$MODNAME
+	$LOWERCASE $INSTALLDIR/steamapps/workshop/content/107410/$MODID
+#	find $MODS/$MODNAME/ -type f -exec chmod -x {} \;
+#	find $MODS/$MODNAME/ -iname "*.bikey" -exec ln -sv {} $UPDATEDKEYS/ \;
+done < $ALLMODIDS
+
+rm $ALLMODIDS
