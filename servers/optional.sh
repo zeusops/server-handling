3036dc4e089365c59eea5d89a313cabf1a66376b
Sami Laine <sami.v.laine@gmail.com>
2018-07-04 18:33:00 +0200

    Add server optional

---
+++ servers/optional.sh
@@ -0,0 +1,12 @@
+NAME=optional
+PORT=2382
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
