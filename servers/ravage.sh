5649eff48b16faa3ee5444b771bd4f9f4c0df459
Sami Laine <sami.v.laine@gmail.com>
2018-09-02 17:49:58 +0200

    Add ravage server, requested by K. Linus

---
+++ servers/ravage.sh
@@ -0,0 +1,13 @@
+# Requested by K. Linus at 2018-08-26
+NAME=ravage
+PORT=2382
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
