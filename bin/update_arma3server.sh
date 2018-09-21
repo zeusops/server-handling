604bc52af549789b94f4b89474c6d20f5f7074f6
Sami Laine <sami.v.laine@gmail.com>
2018-09-21 21:24:38 +0200

    Move Arma 3 update script inside the repo

--- bin/update_arma3server.sh
+++ bin/update_arma3server.sh
@@ -3,22 +3,22 @@
 BASEPATH=$HOME
 STEAMCMD=/usr/games/steamcmd
 
-if [ "$1" = "--test" ];
-then
-	echo "Updating test installation"
-	BASEPATH=$HOME/test
-elif [ "$1" = "--exile" ];
-then
-	echo "Updating exile installation"
-	BASEPATH=$HOME/exile
-else
-	echo "Updating main installation"
-fi
+#if [ "$1" = "--test" ];
+#then
+#	echo "Updating test installation"
+#	BASEPATH=$HOME/test
+#elif [ "$1" = "--exile" ];
+#then
+#	echo "Updating exile installation"
+#	BASEPATH=$HOME/exile
+#else
+echo "Updating main installation"
+#fi
 
-if [ "$1" = "--all" ];
-then
-	$0 --test
-fi
+#if [ "$1" = "--all" ];
+#then
+#	$0 --test
+#fi
 
-$STEAMCMD +runscript $BASEPATH/update_arma3server.txt
+$STEAMCMD +runscript $BASEPATH/files/update_arma3server.txt
 
