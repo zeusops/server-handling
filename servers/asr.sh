6916c23c3f6e21647b86dc93cc1bcb46cfe8fba7
Sami Laine <sami.v.laine@gmail.com>
2018-10-08 00:45:19 +0200

    Add custom servers for Derp's sideop on 2018-10-09

---
+++ servers/asr.sh
@@ -0,0 +1,15 @@
+# Derp's sideop 2018-10-09 https://steamcommunity.com/groups/zeusops/events/1676919997036192604
+NAME=asr
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=asr  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS=-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
---
+++ servers/vcom.sh
@@ -0,0 +1,15 @@
+# Derp's sideop 2018-10-09 https://steamcommunity.com/groups/zeusops/events/1676919997036192604
+NAME=vcom
+PORT=2502
+
+PROFILE=  # default: server
+CONFIG=vcom  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS=-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
