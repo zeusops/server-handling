bc4664d35ce92ff449318d444e409f17a0c0f4e1
Sami Laine <sami.v.laine@gmail.com>
2018-10-07 02:39:16 +0200

    Add a vanilla server for playing the apex campaign

---
+++ servers/apex.sh
@@ -0,0 +1,12 @@
+NAME=apex
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
