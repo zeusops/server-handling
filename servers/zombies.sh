3d32c0554f11ba8bafcce11606c32f800daf07eb
Sami Laine <sami.v.laine@gmail.com>
2018-10-15 09:00:50 +0200

    Add a zombie server, requested by Caboose

---
+++ servers/zombies.sh
@@ -0,0 +1,16 @@
+# 2018-10-14, requested by Caboose
+# https://gyazo.com/c0df33dffac2794a15af22439f9cc1b8
+NAME=zombies
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS= #'-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
