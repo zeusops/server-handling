546e3d6b90c0cefacba0c7321a60772472430b94
Sami Laine <sami.v.laine@gmail.com>
2018-10-25 19:25:59 +0200

    Add server for jointop 2018-10-27

---
+++ servers/jointop.sh
@@ -0,0 +1,14 @@
+NAME=jointop
+PORT=2302
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
