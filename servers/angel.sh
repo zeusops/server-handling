9f175390379dd340a9f22b82d7c40a305effa345
Sami Laine <sami.v.laine@gmail.com>
2018-10-31 20:40:51 +0100

    Add server for Angel's sideop 2018-10-31

---
+++ servers/angel.sh
@@ -0,0 +1,15 @@
+# Angel's sideop at 2018-10-30
+NAME=angel
+PORT=2402
+
+PROFILE=custom  # default: server
+CONFIG=sideop  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
