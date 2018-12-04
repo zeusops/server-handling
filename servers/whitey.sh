8d48f5948a774c934b1dced735fe8df62a1a4097
Sami Laine <sami.v.laine@gmail.com>
2018-12-04 22:35:13 +0100

    Add server for whitey's sideop at 2018-12-03

---
+++ servers/whitey.sh
@@ -0,0 +1,14 @@
+NAME=whitey
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS=-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
