9b9de63900d03138f4eff245e95a90711a95de89
Sami Laine <sami.v.laine@gmail.com>
2018-12-08 23:16:11 +0100

    Replace tabs with spaces

--- bin/update_mods.sh
+++ bin/update_mods.sh
@@ -22,29 +22,29 @@ STEAMCMD=/usr/games/steamcmd
 export AVAILABLEKEYS=$ARMADIR/available_keys/$NAME
 
 function keys {
-	key=$(basename "$1")
-	if [ ! -f $AVAILABLEKEYS/$key ]; then
-		ln -sv $1 $UPDATEDKEYS/$key
-	fi
+  key=$(basename "$1")
+  if [ ! -f $AVAILABLEKEYS/$key ]; then
+    ln -sv $1 $UPDATEDKEYS/$key
+  fi
 }
 
 export -f keys
 
 ALLMODS=""
 while read line; do
-	ARRAY=($line)
-	# File format:
-	# @modname 123456
-	MODID=${ARRAY[1]}
-	ALLMODS="$ALLMODS +workshop_download_item 107410 $MODID validate"
+  ARRAY=($line)
+  # File format:
+  # @modname 123456
+  MODID=${ARRAY[1]}
+  ALLMODS="$ALLMODS +workshop_download_item 107410 $MODID validate"
 done < $MODIDS
 
 if [ "$SKIPDOWNLOAD" != "yes" ]; then
-	echo "Updating mods"
-	$STEAMCMD +login $STEAMUSERNAME +force_install_dir $INSTALLDIR $ALLMODS +quit
-	echo
+  echo "Updating mods"
+  $STEAMCMD +login $STEAMUSERNAME +force_install_dir $INSTALLDIR $ALLMODS +quit
+  echo
 else
-	echo "Updating mod keys. This does not download updates"
+  echo "Updating mod keys. This does not download updates"
 fi
 
 echo "Creating folders"
@@ -59,26 +59,26 @@ find $UPDATEDKEYS -type l -exec rm {} \;
 
 echo "Linking mods"
 while read line; do
-	ARRAY=($line)
-	# File format:
-	# @modname 123456
-	MODNAME=${ARRAY[0]}
-	MODID=${ARRAY[1]}
-	MODPATH=$MODS/$NAME/$MODNAME
-	if [ -e $MODPATH ]; then
-		rm $MODPATH
-	fi
-	if [ ! -e $INSTALLDIR/steamapps/workshop/content/107410/$MODID ];
-	then
-		echo "$MODNAME with ID $MODID missing! Run `basename $0` $NAME"
-		exit 1
-	fi
-	ln -sv $INSTALLDIR/steamapps/workshop/content/107410/$MODID $MODPATH
-	if [ "$SKIPDOWNLOAD" != "yes" ]; then
-		$BASEPATH/files/bin/internal/lowercase_single.sh $MODPATH/
-	fi
-	find $MODPATH/ -type f -exec chmod -x {} \;
-	find $MODPATH/ -iname "*.bikey" -exec bash -c 'keys "$0"' {} \;
+  ARRAY=($line)
+  # File format:
+  # @modname 123456
+  MODNAME=${ARRAY[0]}
+  MODID=${ARRAY[1]}
+  MODPATH=$MODS/$NAME/$MODNAME
+  if [ -e $MODPATH ]; then
+    rm $MODPATH
+  fi
+  if [ ! -e $INSTALLDIR/steamapps/workshop/content/107410/$MODID ];
+  then
+    echo "$MODNAME with ID $MODID missing! Run `basename $0` $NAME"
+    exit 1
+  fi
+  ln -sv $INSTALLDIR/steamapps/workshop/content/107410/$MODID $MODPATH
+  if [ "$SKIPDOWNLOAD" != "yes" ]; then
+    $BASEPATH/files/bin/internal/lowercase_single.sh $MODPATH/
+  fi
+  find $MODPATH/ -type f -exec chmod -x {} \;
+  find $MODPATH/ -iname "*.bikey" -exec bash -c 'keys "$0"' {} \;
 done < $MODIDS
 
 echo "All mods updated. Following keys were updated:"
