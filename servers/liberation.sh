b0172bd93b978cf41d047d1734feaceece2f8afb
Sami Laine <sami.v.laine@gmail.com>
2018-12-27 21:48:10 +0100

    Add a liberation server

---
+++ servers/liberation.sh
@@ -0,0 +1,14 @@
+NAME=liberation
+PORT=2502
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
