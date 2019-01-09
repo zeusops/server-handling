8f8af1ea27c177a400e7b9637f61543ea1a92414
Sami Laine <sami.v.laine@gmail.com>
2019-01-09 13:14:11 +0100

    Add server for Nunes's zombie sideop

--- servers/zombies.sh
+++ servers/zombies.sh
@@ -1,10 +1,11 @@
 # 2018-10-14, requested by Caboose
 # https://gyazo.com/c0df33dffac2794a15af22439f9cc1b8
+# 2019-01-07, sideop by R. Nunes
 NAME=zombies
 PORT=2402
 
 PROFILE=  # default: server
-CONFIG=custom  # default: $NAME
+CONFIG=sideop  # default: $NAME
 PARAMS= #'-checkSignatures'
 
 # Uncomment to prevent automatic key updates
