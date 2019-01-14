dc7a663999291a7b1a69fb4b8a0f23db91060479
Sami Laine <sami.v.laine@gmail.com>
2019-01-14 17:54:20 +0100

    Add Caboose's zombie server

---
+++ servers/caboose.sh
@@ -0,0 +1,13 @@
+# A zombie server, requested by Caboose at 2019-01-14
+NAME=caboose
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=sideop  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
