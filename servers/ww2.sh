6c1dd78fdbe88c1a47dcd99c8154fe16376f5123
Sami Laine <sami.v.laine@gmail.com>
2019-01-14 17:54:41 +0100

    Add WW2 server configuration

---
+++ servers/ww2.sh
@@ -0,0 +1,12 @@
+NAME=ww2
+PORT=2302
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
