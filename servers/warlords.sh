c0ae076ad980efe0a41812c5ec8eb6cff3637d97
Sami Laine <sami.v.laine@gmail.com>
2019-01-28 18:51:07 +0100

    Add a Warlords server

---
+++ servers/warlords.sh
@@ -0,0 +1,12 @@
+NAME=warlords
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=warlords  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
