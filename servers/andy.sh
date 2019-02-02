babba6c0612abc1a73e2ba5d6027e6fdd1a48d9a
Sami Laine <sami.v.laine@gmail.com>
2019-02-02 23:23:21 +0100

    Add server for Andy's OPTRE mission

---
+++ servers/andy.sh
@@ -0,0 +1,12 @@
+NAME=andy
+PORT=2502
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
