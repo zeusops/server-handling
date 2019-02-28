dd1c56802c773a77737a06b959aacafca10ea1b8
Sami Laine <sami.v.laine@gmail.com>
2019-02-28 19:10:30 +0100

    Add Al Rayak Liberation server

---
+++ servers/liberation_al_rayak.sh
@@ -0,0 +1,14 @@
+NAME=liberation_al_rayak
+PORT=2502
+
+PROFILE=liberation_al_rayak  # default: server
+CONFIG=liberation  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
