45b32b1b6581e61f68f909903169609b947564bc
Sami Laine <sami.v.laine@gmail.com>
2019-02-28 19:12:11 +0100

    Add restrepo server

---
+++ servers/restrepo.sh
@@ -0,0 +1,12 @@
+NAME=restrepo
+PORT=2402
+
+PROFILE=restrepo  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=-serverMod=mods/alive/@aliveserver
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
