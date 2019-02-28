7db828517146283fc2e185e08ccbb328d35e065e
Sami Laine <sami.v.laine@gmail.com>
2019-02-28 19:10:00 +0100

    Add alive server

---
+++ servers/alive.sh
@@ -0,0 +1,12 @@
+NAME=alive
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+PARAMS=-serverMod=mods/alive/@aliveserver\;mods/alive/@alive
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
