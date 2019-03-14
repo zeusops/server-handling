a56e71bedd12aec425b30850986d6db29daba7a6
Sami Laine <sami.v.laine@gmail.com>
2019-03-14 11:53:30 +0100

    Add server for Zuzu's WW2 TFAR1.0 sideop

---
+++ servers/sideopww2.sh
@@ -0,0 +1,14 @@
+NAME=sideopww2
+PORT=2302
+
+PROFILE=  # default: server
+CONFIG=sideop  # default: $NAME
+PARAMS=
+
+SERVERMODS=-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
