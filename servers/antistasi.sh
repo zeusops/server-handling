8119c04700748ef956036981bf917809e70aed8e
Sami Laine <sami.v.laine@gmail.com>
2019-07-14 21:33:17 +0200

    Add antistasi server

---
+++ servers/antistasi.sh
@@ -0,0 +1,14 @@
+NAME=antistasi
+PORT=2502
+
+PROFILE=2019-07-08-antistasi # default: server
+CONFIG=antistasi # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
