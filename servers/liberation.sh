e5c1c2eb43a6a07462db29c6a59aeaca415bebb1
Sami Laine <sami.v.laine@gmail.com>
2019-07-31 16:36:31 +0200

    Add new liberation server

--- servers/liberation.sh
+++ servers/liberation.sh
@@ -1,14 +1,14 @@
 NAME=liberation
 PORT=2502
 
-PROFILE=2019-07-17-liberation # default: server
+PROFILE=2019-07-31-contact-liberation # default: server
 CONFIG=liberation  # default: $NAME
 PARAMS='-checkSignatures'
 
 # Uncomment to prevent automatic key updates
 # NOKEYS=on
 
-SERVERMODS= #-serverMod=mods/cntr/@cntr
+SERVERMODS=
 
 # Uncomment to prevent automatic mod detection based on $NAME
 # MODS=-mod=mods/@name
