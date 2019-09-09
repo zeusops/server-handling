7956c2ae0bbdf0ba026341e9baab31c8164d95a7
Sami Laine <sami.v.laine@gmail.com>
2019-09-09 10:21:46 +0200

    Add test server

---
+++ servers/test.sh
@@ -0,0 +1,14 @@
+NAME=test
+PORT=2602
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+# PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+# SERVERMODS=-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
