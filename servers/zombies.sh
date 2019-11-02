9d214e4d32fb43cc30d549dffc594df31c67951d
Sami Laine <sami.v.laine@gmail.com>
2019-11-02 17:16:30 +0100

    Add a server for Caboose's zombie campaign

---
+++ servers/zombies.sh
@@ -0,0 +1,14 @@
+NAME=zombies
+PORT=2402
+
+PROFILE=2019-11-02-zombies  # default: server
+CONFIG=  # default: $NAME
+PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
