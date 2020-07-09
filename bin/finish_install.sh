44da85c19e693b8eda609e80d02df19125637652
Sami Laine <sami.v.laine@aalto.fi>
2020-07-09 21:36:18 +0300

    Attempt to make finish_install platform independent

--- bin/finish_install.sh
+++ bin/finish_install.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-BASEPATH=$HOME
-LINK=$HOME/link
-ARMA=$BASEPATH/arma3
+readonly BASEPATH=$HOME
+readonly BIN=$BASEPATH/files/bin
+. $BIN/internal/platform.sh
+
+readonly LINK=$HOME/link
+readonly ARMA=$BASEPATH/arma3
+readonly AVAILABLEKEYS=$LINK/available_keys
+readonly OPTIONALKEYS=$LINK/optional_keys
+readonly CONFIGLINK=$ARMA/configlink
+
 
 dir=mpmissions
 if [ ! -L $ARMA/$dir ]; then
@@ -17,7 +24,10 @@ if [ ! -L $ARMA/$dir ]; then
     echo "$ARMA/$dir exists"
     exit 1
   fi
-  ln -vs $LINK/$dir $ARMA/$dir
+  if [ ! -d $LINK/$dir ]; then
+    mkdir -p $LINK/$dir
+  fi
+  ln -vs $(readlink -e $LINK/$dir) $ARMA/$dir
 fi
 
 #for dir in A3Antistasi available_keys logs mods optional_keys tmpmissions updated_keys userconfig; do
@@ -29,7 +39,23 @@ for dir in available_keys logs mods optional_keys tmpmissions updated_keys userc
       echo "$ARMA/$dir exists"
       exit 1
     fi
-    ln -vs $LINK/$dir $ARMA/$dir
+    mkdir -p $LINK/$dir
+    ln -vs $(readlink -e $LINK/$dir) $ARMA/$dir
   fi
 done
 
+if [ $PLATFORM = "cygwin" ]; then
+  if [ ! -L $CONFIGLINK ]; then
+    if [ -d $CONFIGLINK ]; then
+      rmdir -v $CONFIGLINK
+    elif [ -e $CONFIGLINK ]; then
+      echo "$CONFIGLINK exists"
+      exit 1
+    fi
+    ln -vs $(readlink -e /cygdrive/c/server/server-handling) $ARMA/configlink
+  fi
+fi
+
+if [ ! -e $OPTIONALKEYS/main ]; then
+  ln -vs $AVAILABLEKEYS/optional $OPTIONALKEYS/main
+fi
---
+++ bin/internal/platform.sh
@@ -0,0 +1,17 @@
+case $(uname -s) in
+  Linux*)
+    if grep -q Microsoft /proc/version; then
+      # WSL on Windows
+      readonly PLATFORM=wls
+      readonly WINDOWS=yes
+    else
+      # Native Linux
+      readonly PLATFORM=linux
+    fi
+  ;;
+  CYGWIN*)
+    # Cygwin on Windows
+    readonly WINDOWS=yes
+    readonly PLATFORM=cygwin
+  ;;
+esac
