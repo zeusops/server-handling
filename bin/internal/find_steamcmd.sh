ecf2a6b7e17f7e3f5e382dfce3e69ecb80161ec9
Sami Laine <sami.v.laine@gmail.com>
2020-07-25 15:39:26 +0100

    Incomplete find_steamcmd

--- bin/internal/find_steamcmd.sh
+++ bin/internal/find_steamcmd.sh
@@ -1,17 +1,29 @@
-case $(uname -s) in
-  Linux*)
-    #if grep -q Microsoft /proc/version; then
-    #  readonly STEAMCMD=/mnt/c/steamcmd/steamcmd.exe
-    #  readonly STEAMINSTALLDIR="$(wslpath -w $INSTALLDIR)"
-    #else
-      readonly STEAMCMD=/usr/games/steamcmd
-      readonly STEAMINSTALLDIR=$INSTALLDIR
-    #fi
-    #readonly INSTALLDIR=$STEAMDIR/mods
+if [ -z "$BIN" ]; then
+  if [ -z "$PLATFORM" ]; then
+    echo "Requires BIN or PLATFORM to be set"
+    exit 1
+  fi
+  if [ -z "$INSTALLDIR" ]; then
+    echo "Requires INSTALLDIR"
+    exit 1
+  fi
+fi
+
+if [ -z "$PLATFORM" ]; then
+  source $BIN/internal/platform.sh
+fi
+
+case $PLATFORM in
+  linux)
+    readonly STEAMCMD=/usr/games/steamcmd
+    readonly STEAMINSTALLDIR=$INSTALLDIR
   ;;
-  CYGWIN*)
+  wls)
+    readonly STEAMCMD=/mnt/c/steamcmd/steamcmd.exe
+    readonly STEAMINSTALLDIR="$(wslpath -w $INSTALLDIR)"
+  ;;
+  cygwin)
     readonly STEAMCMD="cmd.exe /c C:\\steamcmd\\steamcmd.exe"
     readonly STEAMINSTALLDIR="$(cygpath -w $(readlink -e $INSTALLDIR))"
-    readonly WINDOWS=yes
   ;;
 esac
