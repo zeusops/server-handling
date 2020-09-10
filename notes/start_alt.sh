6e90581f3a805cc91ff88ff762432ccdd1140ea6
Sami Laine <sami.v.laine@gmail.com>
2020-09-10 17:08:04 +0100

    Add generic start_alt

--- notes/start_alt.sh
+++ notes/start_alt.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-set -x
 set -e
 
-NAME=sideop
+if [ $# -lt 1 ]; then
+  echo "Usage $(basename $0) NAME [PORT]"
+  echo "Default port: 2402"
+  exit 1
+fi
+
+set -x
+
+NAME=$1
 PROFILE=server_alt
-PORT=2402
+PORT=${2:-2402}
 #CONFIGPATH=server-handling/config/$NAME.cfg
 CONFIGLINK=configlink
 CONFIGPATH=$CONFIGLINK\\config\\$NAME.cfg
@@ -31,8 +38,8 @@ case $(uname -s) in
   ;;
 esac
 
-update_mods.sh optional --skipdl
-update_mods.sh $NAME --skipdl
+update_mods.sh optional --missing
+update_mods.sh $NAME --missing
 
 . $BIN/internal/keys_alt.sh $BASEPATH $NAME
 
