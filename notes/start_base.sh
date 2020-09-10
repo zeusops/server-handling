d036debd3bd7198db7cb24d594b6772d015762a4
Sami Laine <sami.v.laine@gmail.com>
2020-09-10 16:53:54 +0100

    start_base: Handle empty extramods

--- notes/start_base.sh
+++ notes/start_base.sh
@@ -75,5 +75,5 @@ $SERVERPATH/arma3server_x64 \
   -cfg=$BASICPATH \
   -port=$PORT \
   -filePatching \
-  $MODS$EXTRAMODS $SERVERMODS $PARAMS
+  $MODS${EXTRAMODS:-} $SERVERMODS $PARAMS
   #-checkSignatures \
