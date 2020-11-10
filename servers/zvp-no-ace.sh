4d4fcb385be0aa67434e990cf3e801c896900ed9
Sami Laine <sami.v.laine@gmail.com>
2020-11-10 20:53:20 +0100

    Add servers

---
+++ servers/zvp-no-ace.sh
@@ -0,0 +1,14 @@
+NAME=zvp-no-ace
+PORT=2502
+
+PROFILE=  # default: server
+CONFIG=sideop2 # default: $NAME
+#PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
