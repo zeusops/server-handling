632e61f79b158567e2b1815631bb9a9ac1023cca
Sami Laine <sami.v.laine@gmail.com>
2020-11-24 18:38:11 +0100

    Add star wars server

---
+++ servers/starwars.sh
@@ -0,0 +1,14 @@
+NAME=starwars
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+#PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS= #-serverMod=mods/cntr/@cntr
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
