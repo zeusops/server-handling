8f852583615b78efc312af09e8f4c429658a132d
Sami Laine <sami.v.laine@gmail.com>
2021-01-01 23:43:28 +0100

    update-arma3server: Source environment.sh

--- bin/update-arma3server.sh
+++ bin/update-arma3server.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 # cd /home/steam/steamcmd
 
-BASEPATH=$HOME
-INSTALLDIR=$BASEPATH/arma3
-STEAMUSERNAME=zeusoperations
-BIN=$BASEPATH/files/bin
-source $BIN/internal/find-steamcmd.sh
+INSTALL_ARMA=yes
+source ${BASE_PATH:-$HOME/server}/files/bin/internal/environment.sh
 
-#if [ "$1" = "--test" ];
-#then
-#	echo "Updating test installation"
-#	BASEPATH=$HOME/test
-#elif [ "$1" = "--exile" ];
-#then
-#	echo "Updating exile installation"
-#	BASEPATH=$HOME/exile
-#else
 echo "Updating main installation"
-#fi
 
-#if [ "$1" = "--all" ];
-#then
-#	$0 --test
-#fi
+set -x
+$STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR +app_update 233780 -beta creatordlc validate +exit
+set +x
 
-cmd="$STEAMCMD +login $STEAMUSERNAME +force_install_dir $STEAMINSTALLDIR +app_update 233780 -beta creatordlc validate +exit"
-echo $cmd
-$cmd
-
-$BIN/finish-install.sh
+$bin/finish-install.sh
