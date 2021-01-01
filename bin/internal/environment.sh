98886e25e25da6c11ddfaa3ee668802f8e5a020a
Sami Laine <sami.v.laine@gmail.com>
2021-01-01 23:02:38 +0100

    environment.sh: Add $files

--- bin/internal/environment.sh
+++ bin/internal/environment.sh
@@ -1,10 +1,13 @@
+# Sourced by other scripts, not to be executed directly
 readonly base_path=${BASE_PATH:-$HOME/server}
-readonly bin=${BIN:-${base_path}/files/bin}
-readonly armadir=${ARMA_DIR:-${base_path}/base-installation}
-readonly servers=${SERVERS:-${base_path}/files/servers}
+readonly files=${FILES:-$base_path/files}
+readonly bin=${BIN:-$files/bin}
+readonly armadir=${ARMA_DIR:-$base_path/base-installation}
+readonly servers=${SERVERS:-$files/servers}
+readonly log_files=${LOG_FILES:-$HOME/logs}
 
-readonly mod_lists=${MOD_LISTS:-${base_path}/files/modlists}
-readonly lowercase=${LOWERCASE:-${bin}/internal/lowercase-single.sh}
+readonly mod_lists=${MOD_LISTS:-$files/modlists}
+readonly lowercase=${LOWERCASE:-$bin/internal/lowercase-single.sh}
 
 steam_dir=${STEAM_DIR:-$HOME/.steam/steamcmd}
 steam_username=${STEAM_USERNAME:-zeusoperations}
