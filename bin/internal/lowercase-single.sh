7a5b38b65c541fb98982f50fd67cee913fd576c7
Sami Laine <sami.v.laine@gmail.com>
2021-01-01 23:42:30 +0100

    Simplify lowercase-single.sh

--- bin/internal/lowercase-single.sh
+++ bin/internal/lowercase-single.sh
@@ -2,11 +2,12 @@
 if [ ${DEBUG:-no} = "yes" ]; then set -x; fi
 set -euo pipefail
 
-if [ -z ${1} ]
+if [ -z ${1-} ]
 then
-	echo "Usage: `basename \"$0\"` path"
-else
-  if [ -z $WINDOWS ]; then
-	  find $1 -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
-  fi
+  echo "Usage: $(basename $0) path"
+  exit 1
+fi
+
+if [ ${WINDOWS:-no} = "no" ]; then
+  find $1 -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
 fi
