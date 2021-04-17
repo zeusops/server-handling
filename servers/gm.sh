e536e1bfcb5be4ea62f613227e5caaed73d4697e
Sami Laine <sami.v.laine@gmail.com>
2021-04-17 16:39:55 +0200

    Add globmob server

---
+++ servers/gm.sh
@@ -0,0 +1,15 @@
+NAME=gm
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+#PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS="-serverMod=mods/server/@zeusops_server;mods/server/@cntr"
+EXTRAMODS=gm
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
