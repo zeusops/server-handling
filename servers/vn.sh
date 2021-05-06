7574545719d7a82d72cb98dcf1058e8c859f56ff
Sami Laine <sami.v.laine@gmail.com>
2021-05-06 20:48:32 +0200

    Add vn server

---
+++ servers/vn.sh
@@ -0,0 +1,15 @@
+NAME=vn
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+#PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS="-serverMod=mods/server/@zeusops_server;mods/server/@cntr"
+EXTRAMODS=vn
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
