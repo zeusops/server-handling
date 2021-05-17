56a4570ca5f6fa89ec53a7573169afe174358a51
Sami Laine <sami.v.laine@gmail.com>
2021-05-18 00:36:21 +0200

    Update liberation

--- servers/liberation.sh
+++ servers/liberation.sh
@@ -1,14 +1,14 @@
 NAME=liberation
 PORT=2402
 
-PROFILE=2020-12-27-xmas-liberation # default: server
+PROFILE=2021-05-18-liberation # default: server
 CONFIG=  # default: $NAME
 PARAMS='-autoInit'
 
 # Uncomment to prevent automatic key updates
 # NOKEYS=on
 
-SERVERMODS=-serverMod=mods/server/@zeusops_server
+SERVERMODS=  #-serverMod=mods/server/@zeusops_server
 
 # Uncomment to prevent automatic mod detection based on $NAME
 # MODS=-mod=mods/@name
