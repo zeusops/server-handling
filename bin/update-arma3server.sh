441af30a60118802ecf6cbb8d17e32d9b72340c2
Sami Laine <sami.v.laine@gmail.com>
2021-05-21 01:43:03 +0200

    update-arma3server: Add a flag to download the performance branch

--- bin/update-arma3server.sh
+++ bin/update-arma3server.sh
@@ -4,10 +4,19 @@
 INSTALL_ARMA=yes
 source ${BASE_PATH:-$HOME/server}/files/bin/internal/environment.sh
 
+if [ "${1:-}" = "perf" ]; then
+  echo Profiling
+  branch="-beta profiling -betapassword CautionSpecialProfilingAndTestingBranchArma3"
+else
+  echo CDLC
+  branch="-beta creatordlc"
+fi
+
 echo "Updating main installation"
 
+
 set -x
-$STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR +app_update 233780 -beta creatordlc validate +exit
+$STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR +app_update 233780 $branch validate +exit
 set +x
 
 $bin/finish-install.sh
