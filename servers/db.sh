9dfd8f5fed4b1beb32a0c08d29b3b2616af176e5
Sami Laine <sami.v.laine@gmail.com>
2021-05-21 01:48:11 +0200

    Add db test server

---
+++ servers/db.sh
@@ -0,0 +1,14 @@
+NAME=db
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=custom  # default: $NAME
+#PARAMS='-checkSignatures'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+#SERVERMODS="-serverMod=mods/server/@zeusops_server;mods/db/@extdb3"
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=-mod=mods/@name
