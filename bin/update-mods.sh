6930b623ea432cb6177b0f2a6729f962ad6344ff
Sami Laine <sami.v.laine@aalto.fi>
2021-08-02 22:44:22 +0300

    Normalise whitespace

--- bin/update-mods.sh
+++ bin/update-mods.sh
@@ -162,7 +162,7 @@ while read line; do
     else
         allmodids="$allmodids $modid"
     fi
-	allmodids_array+=("$modid")
+  allmodids_array+=("$modid")
 
     #moddlpath=$workshop/$modid
     if [ ! -e $workshop/$modid ]; then
@@ -184,29 +184,29 @@ update_status=$?
 
 
 if [ "$skip_downloads" = "no" ] && ([ "$force_download" = "yes" ] || [ "$update_status" = "1" ]); then
-	did_update="no"
+  did_update="no"
     if [ "$force_download" = "yes" ]; then
-		echo "Updating all mods (forced)..."
-		$STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR $allmods +quit
-		did_update="yes"
-	elif [ "$update_status" = "1" ]; then
-		echo "Updating only updated mods..."
-		for modid in "${allmodids_array[@]}"
-		do
-			python3 ${BASE_PATH:-$HOME/server}/files/bin/checkState.py $path_data/versions_local_state_$name.json ${modid}
-			mod_status=$?
-			if [ "$mod_status" = "1" ]; then
-				echo "Detected update for mod ${modid}, running Steam update."
-				$STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR +workshop_download_item 107410 ${modid} validate +quit
-				did_update="yes"
-			fi
-		done
-	fi
-	if [ "$did_update" = "yes" ]; then
-		# Run the script from the start without downloading to set up symlinks and keys
-		$0 $name --skipdl
-		exit
-	fi
+    echo "Updating all mods (forced)..."
+    $STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR $allmods +quit
+    did_update="yes"
+  elif [ "$update_status" = "1" ]; then
+    echo "Updating only updated mods..."
+    for modid in "${allmodids_array[@]}"
+    do
+      python3 ${BASE_PATH:-$HOME/server}/files/bin/checkState.py $path_data/versions_local_state_$name.json ${modid}
+      mod_status=$?
+      if [ "$mod_status" = "1" ]; then
+        echo "Detected update for mod ${modid}, running Steam update."
+        $STEAMCMD +login $steam_username +force_install_dir $STEAM_INSTALL_DIR +workshop_download_item 107410 ${modid} validate +quit
+        did_update="yes"
+      fi
+    done
+  fi
+  if [ "$did_update" = "yes" ]; then
+    # Run the script from the start without downloading to set up symlinks and keys
+    $0 $name --skipdl
+    exit
+  fi
 else
   echo "Updating mod keys. This does not download updates!"
 fi
