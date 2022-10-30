9088f0657c895413ee85d9430f0c4e2062de4c5b
Sami Laine <sami.v.laine@gmail.com>
2022-10-30 15:38:15 +0000

    Separate server for persistent missiosn

---
+++ servers/missions.sh
@@ -0,0 +1,14 @@
+NAME=missions
+PORT=2402
+
+PROFILE=  # default: server
+CONFIG=  # default: $NAME
+PARAMS='-autoinit'
+
+# Uncomment to prevent automatic key updates
+# NOKEYS=on
+
+SERVERMODS="-serverMod=mods/server/@zeusops_server;@inidbi2"
+
+# Uncomment to prevent automatic mod detection based on $NAME
+# MODS=${MODS}\;mods/@name
