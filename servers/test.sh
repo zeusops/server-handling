88cd4637d5dc78978d07a34fcd3d4b3d8b19909a
Sami Laine <sami.v.laine@gmail.com>
2022-12-28 02:06:53 +0000

    main, test: Enable ocap

--- servers/test.sh
+++ servers/test.sh
@@ -8,7 +8,7 @@ CONFIG=  # default: $NAME
 # Uncomment to prevent automatic key updates
 # NOKEYS=on
 
-SERVERMODS= #-serverMod=mods/cntr/@cntr
+SERVERMODS=-serverMod=mods/server/@ocap
 
 # Uncomment to prevent automatic mod detection based on $NAME
 # MODS=-mod=mods/@name
