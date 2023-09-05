aaaed01b6c5af70f0d320b9874938caf422ff74c
Sami Laine <sami.v.laine@gmail.com>
2023-09-05 18:15:03 +0000

    update-arma3server: Allow updating perf branch without removing cdlc

--- bin/update-arma3server.sh
+++ bin/update-arma3server.sh
@@ -15,25 +15,34 @@ update() {
   if [[ -n "$old_setting" ]]; then set -x; else set +x; fi
 }
 
+update_cdlc() {
+  update "-beta creatordlc"
+}
+
+update_perf() {
+  tmp=$(mktemp -d -p .)
+  mv gm spe csla vn ws "$tmp" || true
+  update "-beta profiling"
+  mv "$tmp/gm" "$tmp/spe" "$tmp/csla" "$tmp/vn" "$tmp/ws" . || true
+  rmdir "$tmp"
+  ln -s arma3server_x64 arma3server_x64_perf
+}
+
 echo "Updating main installation"
 
 case "${1:-}" in
   perf)
-    echo Profiling
-    update "-beta profiling"
+    echo Performance
+    update_perf
     ;;
   cdlc)
     echo CDLC
-    update "-beta creatordlc"
+    update_cdlc
     ;;
   both)
-    echo both
-    tmp=$(mktemp -d -p .)
-    update "-beta creatordlc"
-    mv gm spe csla vn ws "$tmp" || true
-    update "-beta profiling"
-    mv "$tmp/gm" "$tmp/spe" "$tmp/csla" "$tmp/vn" "$tmp/ws" . || true
-    rmdir "$tmp"
+    echo Both
+    update_cdlc
+    update_perf
     ;;
   *)
     echo "Usage: $(basename $0) [cdlc|perf|both]"
